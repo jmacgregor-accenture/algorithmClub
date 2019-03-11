a730908740a71a780018907a964c794713652d0e
John MacGregor <jonathan.macgregor@accenture.com>
2019-03-11 12:50:52 -0400

    Two numbers sorting

--- john-macgregor/bubbleSort/BubblySort/BubblySort/Bubbler.cs
+++ john-macgregor/bubbleSort/BubblySort/BubblySort/Bubbler.cs
@@ -4,9 +4,22 @@ namespace BubblySort
     {
         public int[] Sort(params int[] numbersToSort)
         {
+            var sortedNumbers = new int[numbersToSort.Length];
+            var firstNumber = numbersToSort[0];
+            var secondNumber = numbersToSort[1];
+
+            if (firstNumber > secondNumber)
+            {
+                sortedNumbers[0] = secondNumber;
+                sortedNumbers[1] = firstNumber;
+            }
+            else
+            {
+                sortedNumbers[0] = firstNumber;
+                sortedNumbers[1] = secondNumber;
+            }
             
-            
-            return numbersToSort;
+            return sortedNumbers;
         }
     }
 }
