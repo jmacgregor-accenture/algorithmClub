b0f121cad180b4f783e041bc2a915d2c6507fe69
John MacGregor <jonathan.macgregor@accenture.com>
2019-03-11 13:33:47 -0400

    Four item test passing

--- john-macgregor/bubbleSort/BubblySort/BubblySort.Tests/BubblerTests.cs
+++ john-macgregor/bubbleSort/BubblySort/BubblySort.Tests/BubblerTests.cs
@@ -27,5 +27,27 @@ namespace BubblySort.Tests
             
             result.ShouldBe(expectedArray);
         }
+        
+        [Fact]
+        public void WhenCallingSortWithThreeNumbersReturnsInCorrectOrder()
+        {
+            var bubbler = new Bubbler();
+            var expectedArray = new int[] {5,7,8};
+
+            var result = bubbler.Sort(7, 5, 8);
+            
+            result.ShouldBe(expectedArray);
+        }
+        
+        [Fact]
+        public void WhenCallingSortWithEightNumbersReturnsInCorrectOrder()
+        {
+            var bubbler = new Bubbler();
+            var expectedArray = new int[] {1,2,3,4};
+
+            var result = bubbler.Sort(4,3,1,2);
+            
+            result.ShouldBe(expectedArray);
+        }
     }
 }
--- john-macgregor/bubbleSort/BubblySort/BubblySort/Bubbler.cs
+++ john-macgregor/bubbleSort/BubblySort/BubblySort/Bubbler.cs
@@ -4,22 +4,29 @@ namespace BubblySort
     {
         public int[] Sort(params int[] numbersToSort)
         {
-            var sortedNumbers = new int[numbersToSort.Length];
-            var firstNumber = numbersToSort[0];
-            var secondNumber = numbersToSort[1];
+            var madeChange = false;
 
-            if (firstNumber > secondNumber)
+            do
             {
-                sortedNumbers[0] = secondNumber;
-                sortedNumbers[1] = firstNumber;
-            }
-            else
-            {
-                sortedNumbers[0] = firstNumber;
-                sortedNumbers[1] = secondNumber;
-            }
+                madeChange = false;
+                
+                for (var pass = 0; pass < numbersToSort.Length - 1; pass++)
+                {
+                    var comparisonNumber = pass + 1;
+                
+                    var firstNumber = numbersToSort[pass];
+                    var secondNumber = numbersToSort[comparisonNumber];
+
+                    if (firstNumber > secondNumber)
+                    {
+                        numbersToSort[pass] = secondNumber;
+                        numbersToSort[comparisonNumber] = firstNumber;
+                        madeChange = true;
+                    }
+                }
+            } while (madeChange);
             
-            return sortedNumbers;
+            return numbersToSort;
         }
     }
 }
