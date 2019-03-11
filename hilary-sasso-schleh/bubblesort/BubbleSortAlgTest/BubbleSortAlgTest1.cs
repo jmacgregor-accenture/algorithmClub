d83dc9c2ed1b6c490974707a49f09443f65db2e2
Hilary Sasso-Schleh <hsassoschleh@pillartechnology.com>
2019-03-11 14:51:42 -0400

    Added integer bubblesort and verified would work under multiple array lengths

--- hilary-sasso-schleh/bubblesort/BubbleSortAlgTest/BubbleSortAlgTest1.cs
+++ hilary-sasso-schleh/bubblesort/BubbleSortAlgTest/BubbleSortAlgTest1.cs
@@ -7,7 +7,7 @@ namespace BubbleSortAlgTests
     public class BubbleSortAlgTest1
     {
         [Fact]
-        public void SortReturnsListofIntegersInAscendingOrder()
+        public void SortReturnsTwoIntegersOfArrayInAscendingOrder()
         {
            BubbleSort bubblesort = new BubbleSort();
            int[] numArray = {2, 1};
@@ -16,7 +16,21 @@ namespace BubbleSortAlgTests
            int[] result = bubblesort.Sort(numArray);
          
            Assert.Equal(expected, result);
-           
         }
+        
+        [Fact]
+        public void SortReturnsFiveIntegersInArrayInAscendingOrder()
+        {
+            BubbleSort bubblesort = new BubbleSort();
+            int[] numArray = {2, 1, 3, 5, 4};
+            int[] expected = {1, 2, 3, 4, 5};
+ 
+            int[] result = bubblesort.Sort(numArray);
+         
+            Assert.Equal(expected, result);
+        }
+        
+        
+        
     }
 }
--- hilary-sasso-schleh/bubblesort/BubbleSortAlgo/BubbleSort.cs
+++ hilary-sasso-schleh/bubblesort/BubbleSortAlgo/BubbleSort.cs
@@ -1,11 +1,28 @@
+using System;
+
 namespace BubbleSortAlg
 {
     public class BubbleSort
     {
         public int[] Sort(int[] numArray)
         {
-            numArray = new int[] {1, 2};
-            
+            bool unsorted = true;
+            while (unsorted)
+            {
+                unsorted = false;
+                for (int i = 0; i < numArray.Length - 1; i++)
+                {
+                    int current = numArray[i];
+                    int next = numArray[i + 1];
+                    if (current > next)
+                    {
+                        unsorted = true;
+                        numArray[i] = next;
+                        numArray[i + 1] = current;
+                    }
+                }
+            }
+
             return numArray;
         }
     }
