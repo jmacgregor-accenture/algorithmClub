bfa4eae95ebf802013aa4aacfe20e2eb0a9a8476
John MacGregor <jonathan.macgregor@accenture.com>
2019-03-11 14:34:51 -0400

    Refactor to accept params T[]

--- john-macgregor/bubbleSort/BubblySort/BubblySort/Bubbler.cs
+++ john-macgregor/bubbleSort/BubblySort/BubblySort/Bubbler.cs
@@ -1,12 +1,20 @@
+using System;
+using System.Linq;
+
 namespace BubblySort
 {
     public class Bubbler
     {
         public T[] Sort<T>(params T[] itemsToSort)
         {
+            var typeTtoInt = new Converter<T,int>(input => (int)Convert.ChangeType(input, typeof(T)));
+            var intToTypeT = new Converter<int, T>(input => (T) Convert.ChangeType(input, typeof(T)));
             
+            var intArray = (int[])Array.ConvertAll<T,int>((T[])itemsToSort,typeTtoInt);
+
+            var sortedArray = SortInts(intArray);
             
-            return itemsToSort;
+            return (T[])Array.ConvertAll<int,T>((int[])sortedArray,intToTypeT);
         }
 
         private int[] SortInts(int[] numbersToSort)
