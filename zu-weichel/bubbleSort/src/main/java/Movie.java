ac29633ba6fc524b8f97b4ccf157629792aeb42c
Zuzanna Weichel <zu.weichel@gmail.com>
2019-03-13 13:51:13 -0400

    bubble sort and comparables

---
+++ zu-weichel/bubbleSort/src/main/java/Movie.java
@@ -0,0 +1,6 @@
+public class SortableObject implements Comparable<SortableObject> {
+    @Override
+    public int compareTo(SortableObject o) {
+        return 0;
+    }
+}
---
+++ zu-weichel/bubbleSort/src/main/java/YearComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class YearCompare implements Comparator<Movie> {
+
+    @Override
+    public int compare(Movie m1, Movie m2) {
+        return m2.getYear()-m1.getYear();
+    }
+}
